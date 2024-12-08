0dd35dc8e1a403518022db1247f976986bcf6d55
orestispanago <orestispanago@gmail.com>
2024-12-08 23:30:02 +0300

    Added export to csv in Flights admin

--- flights/admin.py
+++ flights/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
 from .models import Flight, Plane, Mission, Airport
+from django.http import HttpResponse
+import csv
+import datetime
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment;" f"filename={opts.verbose_name}s.csv"
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
 
 
 class FlightsAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Flight._meta.get_fields()]
     readonly_fields = ["air_time"]
+    list_filter = (("date", admin.DateFieldListFilter), "plane", "base")
+    actions = [export_to_csv]
 
 
 class PlanesAdmin(admin.ModelAdmin):
