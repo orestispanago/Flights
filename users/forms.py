01c8ff09e31fac39ed252302c0ffed1e85d0592b
orestispanago <orestispanago@gmail.com>
2024-12-09 14:39:51 +0300

    [FRONT] Using cards in profile, and create flight

--- users/forms.py
+++ users/forms.py
@@ -23,4 +23,4 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["username", "email", "first_name", "last_name"]
